0db272f5b3d13276af16794207d12e39684ab38e
Johan Niklasson <johan@niklasson.me>
2020-10-26 20:48:29 +0100

    Added basic project setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='discrete-integration',
+    version='0.1.0',
+    author="Johan Niklasson",
+    author_email="johan@niklasson.me",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vonNiklasson/discrete-integration",
+    py_modules=['discrete-integration'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        di=discrete-integration/main.py:di
+    ''',
+    classifiers=[],
+    python_requires='>=3.5',
+)

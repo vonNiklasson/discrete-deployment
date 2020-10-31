33b3d71bc2c61dd76c48e8f87c2de4d387199643
Johan Niklasson <johan@niklasson.me>
2020-10-31 22:03:13 +0100

    Moved functions

--- discrete_deployment/utils/file_helper.py
+++ discrete_deployment/utils/file_helper.py
@@ -47,6 +47,26 @@ class FileHelper:
         return os.path.join(path, settings.DD_PATHS_FILE_NAME)
 
     @staticmethod
+    def read_yaml(path: str):
+        with open(path, 'r') as stream:
+            try:
+                return yaml.safe_load(stream)
+            except yaml.YAMLError:
+                return None
+
+    @staticmethod
+    def read_paths_file(path: str):
+        return FileHelper.read_yaml(
+            FileHelper.compose_dd_paths_path(path)
+        )
+
+    @staticmethod
+    def read_config_file(path: str):
+        return FileHelper.read_yaml(
+            FileHelper.compose_dd_config_path(path)
+        )
+
+    @staticmethod
     def find_project_root(current_path: str) -> (str, bool):
         """
         Finds the project root based on the current_path.
@@ -66,23 +86,3 @@ class FileHelper:
             current_path = FileHelper.traverse_up(current_path)
 
         return '', False
-
-    @staticmethod
-    def read_yaml(path: str):
-        with open(path, 'r') as stream:
-            try:
-                return yaml.safe_load(stream)
-            except yaml.YAMLError:
-                return None
-
-    @staticmethod
-    def read_paths_file(path: str):
-        return FileHelper.read_yaml(
-            FileHelper.compose_dd_paths_path(path)
-        )
-
-    @staticmethod
-    def read_config_file(path: str):
-        return FileHelper.read_yaml(
-            FileHelper.compose_dd_config_path(path)
-        )

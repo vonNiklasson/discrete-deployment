3d7c2678ba76e310fe6fce3215bef4344d7b4f99
Johan Niklasson <johan@niklasson.me>
2020-10-31 23:51:14 +0100

    Updated version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='discrete-deployment',
-    version='0.1.2',
+    version='0.1.3',
     author="Johan Niklasson",
     author_email="johan@niklasson.me",
     description="",
